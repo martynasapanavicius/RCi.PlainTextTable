4e9698bb7d92c2897a7f0bd159c99551b3695777
martynasapanavicius <martynas.apanavicius@gmail.com>
2024-11-23 23:46:34 +0200

    sandbox

---
+++ Sandbox/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RCi.PlainTextTable;
+
+namespace Sandbox
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //var nl = Environment.NewLine;
+            var ptt = new Ptt();
+
+            ptt[0, 0].Text("a").RowSpan(2);
+            ptt[0, 1].Text("b");
+            ptt[0, 2].Text("c");
+
+            var str = ptt.ToString();
+            System.Diagnostics.Debug.WriteLine(str);
+            Console.WriteLine(str);
+        }
+    }
+}
