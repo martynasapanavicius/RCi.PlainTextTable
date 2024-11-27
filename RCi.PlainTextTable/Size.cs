cafc91427d644d149c6201a064fde4be58977ecc
martynasapanavicius <martynas.apanavicius@gmail.com>
2024-11-27 02:39:35 +0200

    make Size internal

--- RCi.PlainTextTable/Size.cs
+++ RCi.PlainTextTable/Size.cs
@@ -1,6 +1,6 @@
 ﻿namespace RCi.Toolbox
 {
-    public readonly record struct Size(int Width, int Height)
+    internal readonly record struct Size(int Width, int Height)
     {
         public static implicit operator Size((int Width, int Height) t) => new(t.Width, t.Height);
         public static implicit operator (int Width, int Height)(Size s) => new(s.Width, s.Height);
